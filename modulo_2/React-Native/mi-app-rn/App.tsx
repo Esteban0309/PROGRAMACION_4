001247f52a5d80c58a13485b5bd3c18b16baa5eb
IDIC <alumnos@alumnosuio.ute.edu.ec>
2025-10-27 21:56:22 -0500

    Lunes clase 10-27-2025

---
+++ modulo_2/React-Native/mi-app-rn/App.tsx
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "#0d1117" }}>
+      <Saludo />
+      <Text>Este es el proyecto de Ian Esteban</Text>
+    </View>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
